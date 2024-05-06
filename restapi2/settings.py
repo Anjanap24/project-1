0d5a72138a7f51e8f66d8372f3981438c5dd06a4
anjanap <anjanap028@gmail.com>
2024-05-06 19:01:44 +0530

    c -> mysql

--- restapi2/settings.py
+++ restapi2/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'restapi2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb2',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
